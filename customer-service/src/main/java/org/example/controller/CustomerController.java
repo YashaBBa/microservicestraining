3fab6468c72ee7a99b88c940932bde75b4e7120b
YashaBBa <yashaborovocv@gmail.com>
2023-07-31 11:34:16 +0300

    add and config fraud service and gateway service

--- customer-service/src/main/java/org/example/controller/CustomerController.java
+++ customer-service/src/main/java/org/example/controller/CustomerController.java
@@ -3,18 +3,20 @@ package org.example.controller;
 import org.example.domain.CustomerRegistrationRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.example.service.impl.CustomerServiceImpl;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
 public record CustomerController(CustomerServiceImpl customerService) {
 
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "Hi";
+    }
+
     @PostMapping
-    public void registryNewCustomerRequest(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
-      log.info("new customer registry {}",customerRegistrationRequest);
+    public void registryNewCustomerRequest(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
+        log.info("new customer registry {}", customerRegistrationRequest);
     }
 }
